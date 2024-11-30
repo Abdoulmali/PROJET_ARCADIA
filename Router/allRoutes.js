0c1d2106475e0f5f7d58252f6cf2bf790ca595b1
Abdoulmali <137415411+Abdoulmali@users.noreply.github.com>
2024-11-30 22:23:48 +0100

    Implémentation page lion, léopard , gorille

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,6 +6,9 @@ export const allRoutes = [
   new Route("/habitat", "Visite des habitats", "/pages/habitat.html"),
   new Route("/habitats", "Les habitats", "/pages/habitats.html"),
   new Route("/tigre", "tigre", "/pages/animaux/félins/tigre.html"),
+  new Route("/leopard", "leopard", "/pages/animaux/félins/leopard.html"),
+  new Route("/lion", "lion", "/pages/animaux/félins/lion.html"),
+  new Route("/gorille", "gorille", "/pages/animaux/singes/gorille.html"),
 ];
 
   //Le titre s'affiche comme ceci : Route.titre - websitename
