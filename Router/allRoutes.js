d81a74c02629a20649e37dd707f7777d6c7321be
Abdoulmali <137415411+Abdoulmali@users.noreply.github.com>
2024-12-23 17:38:32 +0100

    signup

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -23,7 +23,9 @@ export const allRoutes = [
   new Route("/restauration", "restauration", "/pages/restauration.html"),
   new Route("/contact", "contact", "/pages/contact.html"),
 
-  new Route("/signin", "Connexion", "/pages/signin.html"),
+  new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+  new Route("/signup", "Deconnexion", "/pages/auth/signup.html"),
+  new Route("/account", "Mon compte", "/pages/auth/account.html"),
 
 
 ];
